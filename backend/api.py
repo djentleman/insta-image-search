e84a40ea3bd2ebe91ad58214fce9746b75b379ed
Todd Perry <todd.perry@myport.ac.uk>
2018-08-24 18:08:23 +0100

    add api with endpoint to accept images

---
+++ backend/api.py
@@ -0,0 +1,20 @@
+import hug
+from client import (
+    IISClient
+)
+from io import (
+    BytesIO
+)
+
+iis_client = IISClient()
+
+
+@hug.post('/upload')
+def upload_file(body):
+    """accepts file uploads"""
+    filename = list(body.keys()).pop()
+    content = body[content]
+    stream = BytesIO(content)
+    response = iis_client.search_with_image(stream)
+    return response
+
