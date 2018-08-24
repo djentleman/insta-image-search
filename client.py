815a10cdd595822697ae16330786beb1303b94b9
Todd Perry <todd.perry@myport.ac.uk>
2018-08-24 17:50:45 +0100

    add client class

---
+++ client.py
@@ -0,0 +1,17 @@
+from model import (
+    do_feature_extraction,
+    get_similarity
+)
+
+# client for handling requests/jobs for the instagram image search
+class IISClient():
+
+    def __init__(self):
+        # load saved images into memory here
+        self.images = []
+
+    def search_with_image(image):
+        vector = do_feature_extraction(image)
+        return sorted(self.images, key=lambda x: -get_similarity(x['vector'], vector))[:10]
+
+
