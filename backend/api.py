20c41cf19552a18837951d58a7ab7533b7567334
nana ogura <>
2018-09-01 23:11:53 +0900

    implement ajax

--- backend/api.py
+++ backend/api.py
@@ -4,12 +4,16 @@ from io import BytesIO
 
 iis_client = IISClient()
 
+api = hug.API(__name__)
+api.http.add_middleware(hug.middleware.CORSMiddleware(api, max_age=10))
 
 @hug.post('/upload')
 def upload_file(body):
     """accepts file uploads"""
-    filename = list(body.keys()).pop()
+    filename = 'file'
     content = body[filename]
+    print(content)
+    if type(content) == str:
+        content = content.encode()
     response = iis_client.search_with_image(BytesIO(content))
     return response
-
---
+++ frontend/javascript/upload.js
@@ -0,0 +1,59 @@
+var Upload = function (file) {
+    this.file = file;
+    console.log($.ajax)
+};
+
+Upload.prototype.getType = function() {
+    return this.file.type;
+};
+Upload.prototype.getSize = function() {
+    return this.file.size;
+};
+Upload.prototype.getName = function() {
+    return this.file.name;
+};
+Upload.prototype.doUpload = function () {
+    var that = this;
+    var formData = new FormData();
+
+    // add assoc key values, this will be posts values
+    formData.append("file", this.file, this.getName());
+    formData.append("upload_file", true);
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8000/upload",
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                myXhr.upload.addEventListener('progress', that.progressHandling, false);
+            }
+            return myXhr;
+        },
+        success: function (data) {
+            console.log(data)
+        },
+        error: function (error) {
+            console.log(error)
+        },
+        async: true,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        timeout: 60000
+    });
+};
+
+Upload.prototype.progressHandling = function (event) {
+    var percent = 0;
+    var position = event.loaded || event.position;
+    var total = event.total;
+    var progress_bar_id = "#progress-wrp";
+    if (event.lengthComputable) {
+        percent = Math.ceil(position / total * 100);
+    }
+    // update progressbars classes so it fits your code
+    $(progress_bar_id + " .progress-bar").css("width", +percent + "%");
+    $(progress_bar_id + " .status").text(percent + "%");
+};
