04d52353aa5aa3dcfe3e4fe12d97f038cc33c307
Todd Perry <todd.perry@myport.ac.uk>
2018-09-01 23:46:18 +0100

    add instagram url to scraper

--- backend/scraper.py
+++ backend/scraper.py
@@ -23,21 +23,27 @@ for hashtag in hashtags:
     url = base_url % (hashtag,)
     images = json.loads(requests.get(url).text)
     for image in images['graphql']['hashtag']['edge_hashtag_to_media']['edges']:
-        image = image['node']
-        if not image['is_video']:
-            # want the 250x250 thumbnail
-            image_url = image['thumbnail_resources'][1]['src']
-            print(image_url)
-            account_id = image['owner']['id']
-            image_data = BytesIO(requests.get(image_url).content)
-            image_vector = model.do_feature_extraction(image_data)
-            data.append({
-                'hashtag': hashtag,
-                'image_url': image_url,
-                'image_vector': image_vector.tolist(),
-                'account_id': account_id
-            })
-        time.sleep(wait)
+        try:
+            image = image['node']
+            if not image['is_video']:
+                # want the 250x250 thumbnail
+                image_url = image['thumbnail_resources'][1]['src']
+                print(image_url)
+                account_id = image['owner']['id']
+                image_data = BytesIO(requests.get(image_url).content)
+                image_vector = model.do_feature_extraction(image_data)
+                shortcode = image['shortcode']
+                instagram_url = 'https://www.instagram.com/p/%s/' % (shortcode,)
+                data.append({
+                    'hashtag': hashtag,
+                    'image_url': image_url,
+                    'instagram_url': instagram_url,
+                    'image_vector': image_vector.tolist(),
+                    'account_id': account_id
+                })
+            time.sleep(wait)
+        except Exception as e:
+            print(e)
     open(output_file % (hashtag,), 'w+').write(json.dumps(data))
     data = []
     time.sleep(wait)
