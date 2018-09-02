a3f354f5135f47d9037865497f718d544babb778
Todd Perry <todd.perry@myport.ac.uk>
2018-09-02 11:54:35 +0100

    add data loader

--- backend/client.py
+++ backend/client.py
@@ -1,7 +1,10 @@
+import os
+import json
 from model import (
     do_feature_extraction,
     get_similarity
 )
+import numpy as np
 
 # client for handling requests/jobs for the instagram image search
 class IISClient():
@@ -9,10 +12,18 @@ class IISClient():
     def __init__(self):
         # load saved images into memory here
         self.images = []
+        self.load_images()
 
     def search_with_image(self, image):
         vector = do_feature_extraction(image)
         print(vector)
         return sorted(self.images, key=lambda x: -get_similarity(x['vector'], vector))[:10]
 
+    def load_images(self):
+        for filename in os.listdir('data/'):
+            print('Loading %s...' % (filename,))
+            data = json.loads(open('data/' + filename, 'r+').read())
+            self.images += data
+        for i in range(len(self.images)):
+            self.images[i]['image_vector'] = np.asfarray(self.images[i]['image_vector'])
 
