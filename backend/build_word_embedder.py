c204b73c37b6381351220e25f04f1841a26f502f
Todd Perry <todd.perry@myport.ac.uk>
2018-12-17 13:59:33 +0000

    build word embedder

--- backend/build_word_embedder.py
+++ backend/build_word_embedder.py
@@ -18,15 +18,17 @@ from random import (
 
 def clean_hashtags(hashtags):
     hashtags =  [h.replace(' ', '') for h in hashtags if h[0] == '#']
+    if len(hashtags) == 0:
+        return []
+    split_hashtags = [ht.split('#') for ht in hashtags]
+    # rejoin
+    hashtags = list(reduce(lambda x, y: x + y, split_hashtags))
+    hashtags = [ht if ht[0] == '#' else '#' + ht for ht in hashtags if ht != '']
     return hashtags
 
 client = IISClient()
 data = [{'iv': i['image_vector'], 'ht': clean_hashtags(i['hashtags'])} for i in client.images]
 data = [d for d in data if len(d['ht']) > 1]
-
-# image vector set - easy
-X = np.array([d['iv'] for d in data])
-
 
 # word vector set - hard, we need to train a word embedding
 hashtags = [d['ht'] for d in data]
@@ -83,7 +85,7 @@ labels = np.array(labels)
 # network hyperparameters
 vector_dim = 300
 epochs = 100
-batch_size = 2048
+batch_size = 100000
 
 
 
@@ -112,25 +114,53 @@ similarity_model = Model(input=[input_target, input_context], output=similarity)
 
 vector_model = Model(input=input_target, output=target)
 
+def save_model(model, path):
+    # serialize model to JSON
+    print("Saving Model...")
+    model_json = model.to_json()
+    with open("%s.json" % path, "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("%s.h5" % path)
+    print("Saved model to disk")
+
+# this will probably need to me moved somwhere else...
+def load_model(path):
+    # load json and create model
+    json_file = open('%s.json' % path, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("%s.h5" % path)
+    print("Loaded model from disk")
+    return loaded_model
+
 # build similarity lambda, and fitting function :)
 def on_epoch_end(epoch, logs):
-    print('Epoch: %s' % epoch)
-    if epoch % 10 != 0:
+    print('Epoch: %d' % (epoch+1))
+    if epoch % 10 != 9:
         print('skipping vaidation...')
         return
     words = [randint(0, vocab_size-1) for i in range(5)]
     for word in words:
         similarities = [(i, similarity_model.predict([np.array([word]), np.array([i])])[0][0][0]) for i in range(vocab_size)]
         similarities = sorted(similarities, key=lambda x: x[1])
-        most_similar = similarities[-10:]
-        print('similar words - %s' % reverse_mapping[str(word)])
-        print(', '.join([reverse_mapping[str(x[0])] for x in most_similar]))
+        most_similar = similarities[-20:-1]
+        least_similar = similarities[:19]
+        print('------------------------------')
+        print('Word: %s' % reverse_mapping[str(word)])
+        print('Similar Words: %s' % ', '.join([reverse_mapping[str(x[0])] for x in most_similar]))
+        print('Disimilar Words: %s' % ', '.join([reverse_mapping[str(x[0])] for x in least_similar]))
+        print('------------------------------')
+
 
 
 
 print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+on_epoch_end(-1, None)
 model.fit([pairs[:, 0], pairs[:, 1]], labels, batch_size=batch_size,
                     epochs=epochs,
                     callbacks=[print_callback])
 
-
+save_model(vector_model, 'word_embedder')'
